c121891d45ef93aed4e0a155f1f2496ea2df5f40
Rafaella Fernández <54990883+chanmi-07@users.noreply.github.com>
2024-05-31 00:03:08 -0500

    chore: Update npm dependencies and project images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,4 @@
-import ItConcept2 from '@assets/images/projects/1.webp';
+import LitleNightmares from '@assets/images/projects/5.webp';
 import SistemaSolarApi from '@assets/images/projects/2.webp';
 import Petcontrado from '@assets/images/projects/3.webp';
 import UbigeosPeru from '@assets/images/projects/4.webp';
@@ -26,6 +26,16 @@ export const projects =
         ]
     },
     {
+        "name": "Little Nightmares (Concepto de diseño)",
+        "description": "Concepto de diseño de la página web del videojuego Little Nightmares. Cuenta con un diseño responsive y view transitions.",
+        "image": LitleNightmares,
+        "url": "https://little-nightmares.vercel.app/",
+        "technologies":
+        [
+            "astro", "tailwindcss", "react"
+        ]
+    },
+    {
         "name": "Ubigeos Perú",
         "description": "API para obtener información de los departamentos, provincias y distritos del Perú.",
         "image": UbigeosPeru,
@@ -34,15 +44,5 @@ export const projects =
         [
             "lumen"
         ]
-    },
-    {
-        "name": "It 2 - Concept",
-        "description": "Información de la película IT Chapter Two diseñado por Andrii Domanchuk.",
-        "image": ItConcept2,
-        "url": "https://graceful-salamander-f77750.netlify.app/",
-        "technologies":
-        [
-            "astro", "tailwindcss", "react"
-        ]
-    },
+    }
 ]
